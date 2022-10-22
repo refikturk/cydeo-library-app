5de2acda460c93ecb76986c4131761b2ef4b0134
refikturk <rfkturk43@gmail.com>
2022-10-22 11:28:29 +0300

    main structure of the task created

---
+++ src/test/java/com/cydeo/library/step_definitions/Hooks.java
@@ -0,0 +1,23 @@
+package com.cydeo.library.step_definitions;
+
+import com.cydeo.library.utilities.Driver;
+import io.cucumber.java.After;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+public class Hooks {
+
+    @After
+    public void teardownScenario(Scenario scenario) { //scenario object is capable to follow scenario steps
+
+        //if scenario fails this condition will be executed
+        if (scenario.isFailed()) {
+            byte[] screenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot, "image/png", scenario.getName());
+        }
+
+    }
+
+
+}
